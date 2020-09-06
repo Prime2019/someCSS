bff0bedd1a5f69da28dadaeb9d80dee06be9509e
Prime2019 <1005448129@qq.com>
2020-09-06 14:18:11 +0800

    测试

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,15 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Vue from "vue";
+import Router from "vue-router";
+import HoverDemo1 from "@/views/hover/demo1.vue";
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: "/",
+      name: "HoverDemo1",
+      component: HoverDemo1
     }
   ]
-})
+});
---
+++ src/views/hover/demo1.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    zheyangzi
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped></style>
